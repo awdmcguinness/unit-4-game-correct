47bb0bc0c3a83d289d8f3c1028b2d3d91286f93d
Anthony McGuinness <amcguinness.re@gmail.com>
2019-11-20 16:27:52 -0500

    fixedchrystal number reset bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   var winCount = 0;
   var lossCount = 0;
-  var compGuess = 0;
-  var blueCrystal = 0;
-  var redCrystal = 0;
-  var GreenCrystal = 0;
-  var orangeCrystal = 0;
-  var crystalNumber = 0;
 
   function reset() {
+    compGuess = 0;
+    blueCrystal = 0;
+    redCrystal = 0;
+    GreenCrystal = 0;
+    orangeCrystal = 0;
+    crystalNumber = 0;
+
     compGuess = Math.floor(Math.random() * 300);
     console.log(compGuess);
     $("#computerGuess").html(compGuess);
@@ -29,7 +30,7 @@ $(document).ready(function() {
     console.log(orangeCrystal);
     $("#orange").attr("value", orangeCrystal);
 
-    var crystalNumber = 0;
+    //var crystalNumber = 0;
     $("#totalCrystalNumber").html(crystalNumber);
   }
 
@@ -54,6 +55,7 @@ $(document).ready(function() {
       console.log("lose");
       lossCount += 1;
       $("#totalLoss").html(lossCount);
+      reset();
     }
   });
 });
